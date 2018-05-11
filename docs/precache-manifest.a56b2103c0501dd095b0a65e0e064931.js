67a41f06a3bfa262cb616d7a0f252b0b09e6629d
Leo <leo@xuebin.me>
2018-05-11 16:48:22 +0800

    build

---
+++ docs/precache-manifest.a56b2103c0501dd095b0a65e0e064931.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "be23e457cf19ab0c967d",
+    "url": "/js/app.be23e457.js"
+  },
+  {
+    "revision": "bc5322b05805563e36d5",
+    "url": "/js/0.bc5322b0.js"
+  },
+  {
+    "revision": "345149fce955b040711f",
+    "url": "/js/1.345149fc.js"
+  },
+  {
+    "revision": "1df02ab1db854ddfaae0",
+    "url": "/js/vendors~app.1df02ab1.js"
+  },
+  {
+    "revision": "00180358555833c79ee3e0cfc3d7b3ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "621bd386841f74e0053cb8e67f8a0604",
+    "url": "/img/ionicons.621bd386.svg"
+  },
+  {
+    "revision": "2c2ae068be3b089e0a5b59abb1831550",
+    "url": "/fonts/ionicons.2c2ae068.eot"
+  },
+  {
+    "revision": "24712f6c47821394fba7942fbb52c3b2",
+    "url": "/fonts/ionicons.24712f6c.ttf"
+  },
+  {
+    "revision": "05acfdb568b3df49ad31355b19495d4a",
+    "url": "/fonts/ionicons.05acfdb5.woff"
+  },
+  {
+    "revision": "1df02ab1db854ddfaae0",
+    "url": "/css/vendors~app.2.4128f2cd.css"
+  },
+  {
+    "revision": "be23e457cf19ab0c967d",
+    "url": "/css/app.c7559a57.css"
+  },
+  {
+    "revision": "345149fce955b040711f",
+    "url": "/css/1.1.952a2c5d.css"
+  }
+];
